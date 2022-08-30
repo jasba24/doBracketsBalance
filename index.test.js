5a13388ed7893bf43488ebdc172a2eb2a2a6ce2e
johan buitrago <johanbuitrago342@gmail.com>
2022-08-30 07:00:54 -0500

    add new use cases

--- index.test.js
+++ index.test.js
@@ -6,19 +6,30 @@ describe('Code Enterview', () => {
     const result = doBracketsBalance('[abc]{')
     expect(expected).toEqual(result)
   })
-  test('Should "[]{}()" return true', () => {
-    const expected = true
-    const result = doBracketsBalance('[]{}()')
-    expect(expected).toEqual(result)
-  })
   test('Should "[a{b[c]d}e{f}g]" return true', () => {
     const expected = true
     const result = doBracketsBalance('[a{b[c]d}e{f}g]')
     expect(expected).toEqual(result)
   })
-  test('Should "Hola mi nombre es}" return false', () => {
+  test('Should "[{}[]])" return false', () => {
     const expected = false
-    const result = doBracketsBalance('Hola mi nombre es}')
+    const result = doBracketsBalance('[{}[]])')
+    expect(expected).toEqual(result)
+  })
+  test('Should "[[[{}]]]" return true', () => {
+    const expected = true
+    const result = doBracketsBalance('[[[{}]]]')
+    expect(expected).toEqual(result)
+  })
+  test('Should "]" return false', () => {
+    const expected = false
+    const result = doBracketsBalance(']')
+    expect(expected).toEqual(result)
+  })
+  test('Should "[}]{" return false', () => {
+    const expected = false
+    const result = doBracketsBalance('[}]{')
     expect(expected).toEqual(result)
   })
 })
+b
