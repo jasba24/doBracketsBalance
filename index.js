8c42955669f6781719ba36ac76efc431883068f4
johan buitrago <johanbuitrago342@gmail.com>
2022-08-30 10:43:58 -0500

    add function to remove the items of the array

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ const splitBrackets = expression => {
   return symbols
 }
 
+const removeItems = (positions, brackets) => {
+  let finalPositions = positions
+  let finalBrackets = brackets
+
+  if (finalPositions[0].length !== 0) {
+    for (let i = 0; i < finalPositions.length; i++) {
+      if (finalPositions[0].findLast(e => e)[1] < finalPositions[1][0][1]) {
+        delete finalBrackets[finalPositions[0].findLast(e => e)[1]]
+        delete finalBrackets[finalPositions[1][0][1]]
+        finalPositions = [[finalPositions[0][0]], [finalPositions[1][1]]]
+      } else {
+        if (finalPositions[1].length > 1) {
+          if (finalPositions[0].findLast(e => e)[1] < finalPositions[1][1][1]) {
+            delete finalBrackets[finalPositions[0].findLast(e => e)[1]]
+            delete finalBrackets[finalPositions[1][1][1]]
+            finalPositions = [[finalPositions[0][0]], [finalPositions[1][0]]]
+          }
+        } else {
+          finalBrackets = false
+        }
+      }
+    }
+    return finalBrackets
+  } else {
+    return finalBrackets
+  }
+}
+
 const identifyOpenBrackets = bracket => {
   const identifier = {
     '[': 1,
